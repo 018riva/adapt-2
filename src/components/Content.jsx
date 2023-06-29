2f4d5096555150f33dc5325d0dba511eed361070
riva <rivich18@gmail.com>
2023-06-30 02:59:57 +0300

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,50 @@
+import './gulp styles/style.css'
+
+export function Content() {
+    return (
+        <div>
+            <header class="header"><h1>Header</h1></header>
+            <div class="container">
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, praesentium.</p>
+                    </div>
+            </div>
+            <div class="grid-container">
+                <div class="grid-item">Div 1</div>
+                <div class="grid-item">Div 2</div>
+            </div>
+        </div>
+        </div>
+    )
+}
